302eab37bf272f41ac31b68e57daf99c86b7fba1
Dayton Schuh <daytonschuh@gmail.com>
2021-03-16 11:29:37 -0700

    Added Photo and DateTimeExtension; Updated AppUser

---
+++ ModelLayer/Photo.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelLayer
+{
+    [Table("Photos")]
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
