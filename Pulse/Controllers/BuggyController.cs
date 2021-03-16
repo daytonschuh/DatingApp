5ac87a47c2623570c4ad7045a3b7a4130e774e79
Dayton Schuh <daytonschuh@gmail.com>
2021-03-16 08:08:22 -0700

    Exception Handling

---
+++ Pulse/Controllers/BuggyController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+using ModelLayer;
+using BusinessLogicLayer;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly BusinessLogicClass _businessLogicClass;
+        public BuggyController(BusinessLogicClass businessLogicClass)
+        {
+            _businessLogicClass = businessLogicClass;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public async Task<ActionResult<string>> GetNotFound()
+        {
+            var thing = await _businessLogicClass.GetUser(-1);
+
+            if (thing == null) return NotFound();
+
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public async Task<ActionResult<string>> GetServerError()
+        {
+            var thing = await _businessLogicClass.GetUser(-1);
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+    }
+}
